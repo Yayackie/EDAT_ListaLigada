b8ca64563180bc8ce5ed6c6073699c1cd94f4e24
Yayackie <23030393@itesa.edu.mx>
2024-10-28 09:17:53 -0600

    Versión_03: Se agregó el menu de opciones.

--- ListaLigada.Ui.Consola/Program.cs
+++ ListaLigada.Ui.Consola/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ListaLigada.Logica;
+using System.ComponentModel.Design;
 
 internal class Program
 {
@@ -9,8 +10,80 @@ internal class Program
 
         var singleList = new SingleList<String>();
 
+        int opcion = 0;
+
+        do 
+        {
+            opcion = Menu();
+
+            switch (opcion) {
+                case 1:
+                    AgregarElemento();
+                    break;
+
+                case 2:
+                    MostrarLista();
+                    break;
+
+                case 0:
+                    
+                    break;
+                
+                default:
+                    Console.WriteLine("Esta opcion no existe");
+                    break;
+            
+            }
+
+
+
+        } while (opcion != 0);
+
+        //Se puede quitar private static y el metodo puede crearse de manera directa dentro del main
+        //para evitar crear un nuevo objeto una y otra vez
+        void AgregarElemento()
+        {
+            Console.WriteLine("Escribe el nombre del nodo");
+            var data = Console.ReadLine();
+            singleList.Add(data);
+
+        }
+
+        void MostrarLista() {
+            Console.WriteLine(singleList);
+        }
 
 
         Console.ReadKey();
+    } //Termina el main
+
+
+    private static int Menu()
+    {
+        Console.WriteLine("\nMenu\n");
+        Console.WriteLine("1.- Agregar elemento");
+        Console.WriteLine("2.- Mostrar lista");
+        Console.WriteLine("0.- Salir\n");
+
+        bool esValido = false;
+        //Se puede poner ! en una propiedad, pero como es variable, no hay problema
+        int opcion = 0;
+
+        do 
+        {
+            Console.WriteLine("Elige una opcion del menu");
+            var opcionCapturada = Console.ReadLine();
+
+            if (!int.TryParse(opcionCapturada, out opcion) || opcion < 0 || opcion > 2)
+            {
+                Console.WriteLine("Digite una opcion valida");
+                esValido = false;
+            }
+            else {
+                esValido = true;
+            }
+
+        } while (esValido == false);
+        return opcion;
     }
 }
